867e0dfde2de3637ed218fee9dd2c3b1b7b1cec5
Harini-murugesh <143285013+Harini-murugesh@users.noreply.github.com>
2025-09-09 10:04:07 +0530

    operators

---
+++ JAVASCRIPT/operators.js
@@ -0,0 +1,23 @@
+let a= 10
+let b=2
+let c= 5
+console.log("a=",a,"b=",b);
+//arithmetic
+console.log("addition",a+b);
+console.log("subtraction",a-b);
+console.log("division",a/b);
+console.log("modulus",a%b);
+console.log("exponentation",a**b);
+console.log("increment",c++);
+console.log(c);
+
+//assignment
+c += 10
+console.log(c);
+a **= 2; 
+console.log(a);
+
+console.log(a===b);
+console.log(7 > 3);
+console.log(7 <= 8)
+
